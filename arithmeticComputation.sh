c79965363299dede0a0c3b69f1448f5f61e0c0d5
Shubham Kumar <swayamshubham007.sm@gmail.com>
2020-07-06 08:24:58 +0530

    Update arithmeticComputation.sh

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -7,38 +7,38 @@ read -p "Enter the second number: " secondNumber
 read -p "Enter the third number: " thirdNumber
 #Operations being performed.
 result1=$((firstNumber+secondNumber*thirdNumber))
-	echo "$firstNumber + $secondNumber * $thirdNumber = $result1"
+        echo "$firstNumber + $secondNumber * $thirdNumber = $result1"
 result2=$((firstNumber*secondNumber+thirdNumber))
-	echo "$firstNumber * $secondNumber + $thirdNumber = $result2"
-result3=`echo "scale=2; $thirdNumber+$firstNumber/$secondNumber" |bc`
-	echo "$thirdNumber * $firstNumber / $secondNumber = $result3"
+        echo "$firstNumber * $secondNumber + $thirdNumber = $result2"
+result3=$((thirdNumber+firstNumber/secondNumber))
+        echo "$thirdNumber * $firstNumber / $secondNumber = $result3"
 result4=$((firstNumber%secondNumber+thirdNumber))
-	echo "$firstNumber % $secondNumber + $thirdNumber = $result4"
+        echo "$firstNumber % $secondNumber + $thirdNumber = $result4"
 #Declaring Dictionary and displaying it.
 declare -A result
-	result[computation1]=$result1
-	result[computation2]=$result2
-	result[computation3]=$result3
-	result[computation4]=$result4
+        result[computation1]=$result1
+        result[computation2]=$result2
+        result[computation3]=$result3
+        result[computation4]=$result4
 echo ${result[@]}
 #Declaring and displaying array.
 for (( index=0; index<${#result[@]}; index++ ))
 do
-	Array[index]=${result[computation$((index+1))]}
+        Array[index]=${result[computation$((index+1))]}
 done
 echo ${Array[@]}
 #Sorting in decending order and displaying it.
 for (( index1=0; index1<${#Array[@]}; index1++ ))
 do
-	for (( index2=index1+1; index2<${#Array[@]}; index2++ ))
-	do
-		if (($(echo "${Array[index1]} < ${Array[index2]}"| bc -l)))
-		then
-			temporary=${Array[index1]}
-			Array[index1]=${Array[index2]}
-			Array[index2]=$temporary
-		fi
-	done
+        for (( index2=index1+1; index2<${#Array[@]}; index2++ ))
+        do
+                if (( ${Array[index1]} < ${Array[index2]} ))
+                then
+                        temporary=${Array[index1]}
+                        Array[index1]=${Array[index2]}
+                        Array[index2]=$temporary
+                fi
+        done
 done
 echo "Array in Decending order will be like: ${Array[@]}"
 #Sorting in ascending order.
@@ -46,7 +46,7 @@ for (( index1=0; index1<${#Array[@]}; index1++ ))
 do
    for (( index2=index1+1; index2<${#Array[@]}; index2++ ))
    do
-      if (($(echo "${Array[index1]} > ${Array[index2]}"| bc -l)))
+      if (( ${Array[index1]} > ${Array[index2]} ))
       then
          temporary=${Array[index2]}
          Array[index2]=${Array[index1]}
@@ -55,3 +55,4 @@ do
    done
 done
 echo "Array in ascending order will be like: ${Array[@]}"
+
